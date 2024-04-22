49d3b273b3ebf7e57de8ee9496300ac36a76d64e
joaquimmulaza <joaquimmulazadev@gmail.com>
2024-04-22 16:44:43 +0100

    add swipper slide

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
