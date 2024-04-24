b21d90ccd5185a7d8181bf31d454fb76fa039a84
joaquimmulaza <joaquimmulazadev@gmail.com>
2024-04-24 16:47:35 +0100

    create new slides

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,65 @@ var swiper2 = new Swiper(".mySwiper2", {
     swiper: swiper,
   },
 });
+
+var swipper4 = new Swiper(".mySwiper4", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+var swipper3 = new Swiper(".mySwiper3", {
+  loop: true,
+  spaceBetween: true,
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swipper4,
+  },
+});
+
+//MOBILE
+var swipper5 = new Swiper(".mySwiper5", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+var swipper6 = new Swiper(".mySwiper6", {
+  loop: true,
+  spaceBetween: true,
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swipper5,
+  },
+});
+
+//ELECTRODOMESTICO
+var swipper7 = new Swiper(".mySwiper7", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+var swipper8 = new Swiper(".mySwiper8", {
+  loop: true,
+  spaceBetween: true,
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swipper7,
+  },
+});
