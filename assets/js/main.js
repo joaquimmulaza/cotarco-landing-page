984f5dce1f28140ad8ce3a3c87fffe80063f026d
joaquimmulaza <joaquimmulazadev@gmail.com>
2024-04-26 16:59:28 +0100

    atualização com erros

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,3 +81,14 @@ var swipper8 = new Swiper(".mySwiper8", {
 });
 
 
+const button = document.querySelector(".button button");
+const modal = document.querySelector("dialog")
+const buttonClose = document.querySelector("dialog button")
+
+button.onclick = function () {
+modal.showModal()
+}
+
+ buttonClose.onclick = function () {
+   modal.close()
+}
